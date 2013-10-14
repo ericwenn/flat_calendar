1c88865c5218cae0925fa7a46d76fc24ff28ad14
Eric Wennerberg <ericwenn@gmail.com>
2013-10-14 20:57:48 +0200

    v.1.1.0 release

--- calendar.js
+++ calendar.js
@@ -3,10 +3,11 @@ var calendarArray =[];
 var monthOffset = [6,7,8,9,10,11,0,1,2,3,4,5];
 var monthArray = [["JAN","January"],["FEB","February"],["MAR","March"],["APR","April"],["MAY","May"],["JUN","June"],["JUL","July"],["AUG","August"],["SEP","September"],["OCT","October"],["NOV","November"],["DEC","December"]];
 var dayArray = ["7","1","2","3","4","5","6"];
+var dayName =["M","T","W","T","F","S","S"];
 $(document).ready(function() {
-	$(document).on('click','.calendar-day.have-events',activateDay);
+	$(document).on('click','.c-day.have-events',activateDay);
 	$(document).on('click','.specific-day',activatecalendar);
-	$(document).on('click','.calendar-month-view-arrow',offsetcalendar);
+	$(document).on('click','.c-month-arrow',offsetcalendar);
 	$(window).resize(calendarScale);
 	$(".calendar").calendar({
 		"2013910": {
@@ -39,8 +40,6 @@ $(document).ready(function() {
 				});
 				calendarArray[date] = tempdayarray;
 			});
-
-        
         return this;
     }; 
 }( jQuery ));
@@ -90,7 +89,7 @@ $(document).ready(function() {
 	}
 
 	function calendarSet() {
-		$(".calendar").append('<div class="calendar-month-view"><div class="calendar-month-view-arrow" data-dir="left">‹</div><p></p><div class="calendar-month-view-arrow" data-dir="right">›</div></div><div class="calendar-holder"><div class="calendar-grid"></div><div class="calendar-specific"><div class="specific-day"><div class="specific-day-info" i="day"></div><div class="specific-day-info" i="month"></div></div><div class="specific-day-scheme"></div></div></div>');
+		$(".calendar").append('<div class="c-month-view"><div class="c-month-arrow" data-dir="left">‹</div><p></p><div class="c-month-arrow" data-dir="right">›</div></div><div class="c-holder"><div class="c-grid"></div><div class="c-specific"><div class="specific-day"><div class="specific-day-info" i="day"></div><div class="specific-day-info" i="month"></div></div><div class="s-scheme"></div></div></div>');
 		$(".calendar").each(function() {
 			if($(this).data("color") == undefined) {
 				$(this).data("color","red");
@@ -133,7 +132,7 @@ $(document).ready(function() {
 		if(this.events !== undefined) {
 		var ev = orderBy('start',this.events);
 		for(var o = 0; o<ev.length;o++) {
-			$(".specific-day-scheme").append('<div class="specific-day-scheme-event"><h1>'+ev[o]['name']+'</h1><p data-role="dur">'+ev[o]['start']+' - '+ev[o]['end']+'</p><p data-role="loc">'+ev[o]['location']+'</p></div>');
+			$(".s-scheme").append('<div class="s-event"><h1>'+ev[o]['name']+'</h1><p data-role="dur">'+ev[o]['start']+' - '+ev[o]['end']+'</p><p data-role="loc">'+ev[o]['location']+'</p></div>');
 		}
 		}
 	}
@@ -142,7 +141,7 @@ $(document).ready(function() {
 		$(".specific-day-scheme").html('');
 	}
 	function calendarSetMonth(ele,offset) {
-		ele.find(".calendar-grid").html('');
+		ele.find(".c-grid").html('');
 		var d = new Date();
 		var c = new Date();
 		var e = new Date();
@@ -153,37 +152,53 @@ $(document).ready(function() {
 		} else {
 			ele.attr('offset', 0);
 		}
-		ele.find(".calendar-month-view p").text(monthArray[d.getMonth()][1]+' '+d.getFullYear());
+		ele.find(".c-month-view p").text(monthArray[d.getMonth()][1]+' '+d.getFullYear());
 			d.setDate(1);
 			if(dayArray[d.getDay()] == 1) {
 				d.setDate(d.getDate()-7);
 			} else {
 				d.setDate(d.getDate()-dayArray[d.getDay()]+1);
 			}
+			var crow;
+			if(ele.data('showdays')) {
+				ele.find('.c-grid').append('<div class="c-row"><div class="c-day c-l"><div class="date-holder">'+dayName[0]+'</div></div><div class="c-day c-l"><div class="date-holder">'+dayName[1]+'</div></div><div class="c-day c-l"><div class="date-holder">'+dayName[2]+'</div></div><div class="c-day c-l"><div class="date-holder">'+dayName[3]+'</div></div><div class="c-day c-l"><div class="date-holder">'+dayName[4]+'</div></div><div class="c-day c-l"><div class="date-holder">'+dayName[5]+'</div></div><div class="c-day c-l"><div class="date-holder">'+dayName[6]+'</div></div></div>');
+			}
 			for(var i=0;i<42;i++) {
+				if(i==0 || i==7 || i==14 || i==21 || i==28 || i==35) {
+					crow = $('<div class="c-row"></div>');
+					ele.find('.c-holder .c-grid').append(crow);
+				}
 				d.setDate(d.getDate()+i);
-				var cal_day = $('<div class="calendar-day"><div class="date-holder">'+d.getDate()+'</div></div>');
+				var cal_day = $('<div class="c-day"><div class="date-holder">'+d.getDate()+'</div></div>');
 				if(d.getMonth() !== e.getMonth()) {
 					cal_day.addClass('other-month');
 				}
 				if(d.getTime() == c.getTime()) {
 					cal_day.addClass('this-day');
 				}
-				var strtime = d.getFullYear()+''+(d.getMonth()+1)+''+d.getDate();
+				var strtime = dToString(d);
 				if(calendarArray[strtime] !== undefined) {
 					cal_day.addClass('have-events');
 				}
-				var cal_day_eventholder = $('<div class="event-notif-holder"></div>');
+				var cal_day_eventholder = $('<div class="event-n-holder"></div>');
 				if(calendarArray[strtime] != undefined) {
 					for(var u=0;u<3 && u<calendarArray[strtime].length;u++) {
-						cal_day_eventholder.append('<div class="event-notif"></div>')
+						cal_day_eventholder.append('<div class="event-n"></div>')
 					}
 				}
 				cal_day.attr('strtime',strtime);
 				cal_day.attr('time',d.getTime());
 				cal_day.prepend(cal_day_eventholder);
 
-				ele.find(".calendar-grid").append(cal_day);
+				crow.append(cal_day);
 				d.setDate(d.getDate()-i);
 			}
+	}
+	function dToString(d) {
+		var y = d.getFullYear();
+		var m = d.getMonth()+1;
+		var da = d.getDate();
+		if(m.toString().length<2) m = "0"+m;
+		if(da.toString().length<2) da = "0"+da;
+		return y+''+m+''+da;
 	}
---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />	
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+	<link href="http://www.ericwenn.se/css/basic.css" rel="stylesheet">
+	<script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+	<script src="http://www.ericwenn.se/js/basic.js"></script>
+
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@ericwenn">
+	<meta name="twitter:creator" content="@ericwenn">
+	<meta name="twitter:title" content="Flatmin Calendar Widget - Eric Wennerberg">
+	<meta name="twitter:description" content="A flat & responsive calendar widget. This widget is a part of Eric Wennerbergs Flatmin Framework but works just as good in this standalone download. ">
+
+	<title>Flatmin Calendar Widget - Eric Wennerberg</title>
+	<link href="c.css" rel="stylesheet">
+	<script src="calendar.js"></script>
+	<link href="http://www.ericwenn.se/frameworks/css/frameworks.css" rel="stylesheet">
+	<script src="http://www.ericwenn.se/frameworks/js/frameworks.js"></script>
+	<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
+</head>
+<style>
+* {
+	box-sizing:border-box;
+}
+</style>
+<?php
+mysql_connect("mysql.u0056300.fsdata.se", "u0056300", "mimmsan123") or die(mysql_error());
+mysql_select_db("u0056300_e") or die(mysql_error());
+mysql_set_charset('utf8');
+$url = "http://ericwenn.se/frameworks/flatmin/calendar/";
+$result = mysql_query("SELECT * FROM E_PLAYGROUND WHERE http='$url'");
+$row = mysql_fetch_array($result);
+$version = $row['version'];
+$dllink = $row['download'];
+?>
+<body>
+<?php
+include('/home/u/u0056300/www/ericwenn/php/basic/header.php');
+?>
+<div id="content">
+<div class="framework-container">
+<h1>Flatmin responsive calendar</h1>
+<p>This widget is a part of Flatmin Responsive Framework by <a href="http://www.ericwenn.se" class="inline-style"><span data-text="Eric Wennerberg">Eric Wennerberg</span></a></p>
+<div opn="share" class="share-bar">
+<div class="g-plus" data-action="share" data-annotation="bubble"></div><a href="https://twitter.com/ericwenn" class="twitter-follow-button" data-show-count="false">Follow @ericwenn</a><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://ericwenn.se/frameworks/flatmin/calendar/" data-via="ericwenn" data-text="A flat, responsive and awesome calendar widget" data-lang="en">Tweet</a>
+</div>
+<div class="box calendar" data-showDays="true" data-color="red" opn="demo" style="height:20em";>
+	<div data-role="day" data-day="20130915">
+		<div data-role="event" data-name="Meet the boss" data-start="8.00" data-end="9.00" data-location="London"></div>
+		<div data-role="event" data-name="Cry because of the meeting" data-start="9.30" data-end="10.00" data-location="London"></div>
+	</div>
+	<div data-role="day" data-day="20130901">
+		<div data-role="event" data-name="Walk the dog" data-start="8.00" data-end="9.00" data-location="London"></div>
+		<div data-role="event" data-name="Take doggie to the vet" data-start="9.30" data-end="10.00" data-location="London"></div>
+		<div data-role="event" data-name="Visit the store" data-start="10.30" data-end="11.00" data-location="London"></div>
+	</div>
+	<div data-role="day" data-day="20130910">
+		<div data-role="event" data-name="Go to the game with Felicia" data-start="18.00" data-end="23.00" data-location="London"></div>
+		<div data-role="event" data-name="Walk the dog" data-start="15.00" data-end="16.00" data-location="London"></div>
+	</div>
+	<div data-role="day" data-day="20131010">
+		<div data-role="event" data-name="Go to the game with Felicia" data-start="18.00" data-end="23.00" data-location="London"></div>
+		<div data-role="event" data-name="Walk the dog" data-start="15.00" data-end="16.00" data-location="London"></div>
+	</div>
+	<div data-role="day" data-day="20131015">
+		<div data-role="event" data-name="Meet the boss" data-start="8.00" data-end="9.00" data-location="London"></div>
+		<div data-role="event" data-name="Cry because of the meeting" data-start="9.30" data-end="10.00" data-location="London"></div>
+	</div>
+	<div data-role="day" data-day="20131001">
+		<div data-role="event" data-name="Walk the dog" data-start="8.00" data-end="9.00" data-location="London"></div>
+		<div data-role="event" data-name="Take doggie to the vet" data-start="9.30" data-end="10.00" data-location="London"></div>
+		<div data-role="event" data-name="Visit the store" data-start="10.30" data-end="11.00" data-location="London"></div>
+	</div>
+	<div data-role="day" data-day="20131002">
+		<div data-role="event" data-name="Go to the game with Felicia" data-start="18.00" data-end="23.00" data-location="London"></div>
+		<div data-role="event" data-name="Walk the dog" data-start="15.00" data-end="16.00" data-location="London"></div>
+	</div>
+</div>
+<p class="jquery-need">This widget is dependent on Jquery</p>
+<a opn="download" href="<?php echo $dllink; ?>" class="nostyle"><div class="downloadbutton" data-version="<?php echo $version; ?>" data-product="Flatmin Calendar"><span>Download v<?php echo $version; ?></span></div></a>
+<p>If you like this plugin and want to follow the development, follow me on twitter<p>
+<a href="https://twitter.com/ericwenn" class="twitter-follow-button" data-show-count="false">Follow @ericwenn</a>
+<pre><code class="prettyprint">&lt;link href=&quot;http://www.ericwenn.se/frameworks/flatmin/calendar/calendar.css&quot; rel=&quot;stylesheet&quot;&gt;<br/>&lt;script src=&quot;http://www.ericwenn.se/frameworks/flatmin/calendar/calendar.js&quot;&gt;&lt;/script&gt;</code></pre>
+<h2>Changelog v1.1.0</h2>1
+<ul>
+	<li><h5>Flexbox</h5><p>The plugin is now harnessing the full power of flexbox. But dont worry its compitable with old browsers aswell.</p></li>
+	<li><h5>Dateformat</h5><p>In this version you must use the format YYYYMMDD, example 20130101. This is in order to fix problems with some months. Thanks to <a class="inline-style" href="http://webfro.gs/">web frogs</a> for pointing this out.</p></li>
+	<li><h5>Name of days</h5><p>Just as shown in the example above you can now option to show the name of the days. Check out usage section to learn how.</p></li>
+	<li><h5>Sass File</h5><p>Included in the zip is a SASS-file in order to make customizations easier.</p></li>
+</ul>
+<h2 opn="features">Features</h2>
+<ul>
+	<li><h5>Adaptable</h5><p>The widget will look great, no matter the width of the container.</p></li>
+	<li><h5>Color Swatches</h5><p>The css file contains code to change between <a href="#colorswatches">4 color swatches</a>. Its also possible to add your own color with a little css-knowledge.
+	<li><h5>Fast Load</h5><p>Months are loaded/unloaded as theyre toggled on/off.</p></li>
+	<li><h5>Small Size</h5><p>Combined filesize is ~10kb.
+	<li><h5>No Month Range</h5><p>The only limitation is before 1970 (Epoch). Everything else can be displayed. </p></li>
+	<li><h5>Events</h5><p>Easily add events with certain properties such as starting time, ending time, location and name.</li>
+	<li><h5>Customizable Style</h5><p>Possible to style it with the help of the included css files.</p></li>
+</ul>
+<h2 opn="usage">Usage</h2>
+<ul>
+	<li><h5>Simple calendar</h5><p><pre><code class="prettyprint"> &lt;div class=&quot;calendar&quot;&gt;&lt;/div&gt; </code></pre><p>This will result in a calendar without any events.</p></p></li>
+	<li><h5>Show days</h5><p>In order to show name of the days add the data showdays="true" to the calendar element. Like this:
+	<pre><code class="prettyprint"> &lt;div class=&quot;calendar&quot; data-showdays=&quot;true&quot; &gt;&lt;/div&gt; </code></pre></p></li>
+	<li><h5>Calendar with events</h5>
+	<p>
+	<pre><code class="prettyprint">&lt;div class=&quot;calendar&quot;&gt;<br/>	&lt;div data-role=&quot;day&quot; data-day=&quot;2013910&quot;&gt;<br/>		&lt;div data-role=&quot;event&quot; data-name=&quot;This is an event&quot; data-start=&quot;9.00&quot; data-end=&quot;9.30&quot; data-location=&quot;The Web&quot;&gt;&lt;/div&gt;<br/>		&lt;div data-role=&quot;event&quot; data-name=&quot;This is also an event&quot; data-start=&quot;10.00&quot; data-end=&quot;11.00&quot; data-location=&quot;At Home&quot;&gt;&lt;/div&gt;<br/>	&lt;/div&gt;<br/>&lt;/div&gt;</code></pre>
+	<p>Within the calendar container you can add events to be processed by the script. Each event must be wrapped in a container together with other events on the same day just as in the example.</p>
+	<p>The data-day value should containg leading zeroes.</p>
+	<pre><code class="prettyprint">&lt;div data-role=&quot;day&quot; data-day=&quot;201311&quot;&gt;</code></pre>
+	<p>This is incorrect and should instead be:</p>
+	<pre><code class="prettyprint">&lt;div data-role=&quot;day&quot; data-day=&quot;20130101&quot;&gt;</code></pre>
+	</p>
+</ul>
+<div id="colorswatches">
+<h2 opn="colorswatches">Color Swatches</h2>
+	<div class="color"><div class="color-inset" style="background:#E74C3C;">red</div><div class="color-inset" style="background-color: #C0392B;"></div></div><div class="color"><div class="color-inset" style="background:#3498DB;">blue</div><div class="color-inset" style="background-color: #2980B9;"></div></div><div class="color"><div class="color-inset" style="background:#2ECC71;">green</div><div class="color-inset" style="background-color: #27AE60;"></div></div><div class="color"><div class="color-inset" style="background:#F1C40F;">yellow</div><div class="color-inset" style="background-color: #F39C12;"></div></div>
+	<pre><code class="prettyprint"> &lt;div class=&quot;calendar&quot; data-color=&quot;red&quot; &gt;&lt;/div&gt; </code></pre>
+	<p>This will result in a red calendar, like the one on the top of this page. The color is set to red by default.</p>
+</div>
+<a href="<?php echo $dllink; ?>" class="nostyle"><div class="downloadbutton" data-version="<?php echo $version; ?>" data-product="Flatmin Calendar"><span>Download v<?php echo $version; ?></span></div></a>
+</div>
+</div>
+</body>
+</html>
